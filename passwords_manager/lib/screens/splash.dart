4753cce9f4177f11b2e4a22c99354bdd7bf7f36c
fatmakhadidja <154927228+fatmakhadidja@users.noreply.github.com>
2025-03-18 21:31:38 +0100

    splash screen UI

--- passwords_manager/lib/screens/splash.dart
+++ passwords_manager/lib/screens/splash.dart
@@ -1,1 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:passwords_manager/theme/theme_constants.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(), // Pushes everything below it down
+          Center(
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png'),
+                SizedBox(height: 10),
+                RichText(
+                  text: TextSpan(
+                    text: 'ONE ',
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontFamily: 'BabasNeue',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 24,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'PASS',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(), // Pushes the text below to the bottom
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'The only password manager you’ll ever need.',
+              style: Theme.of(context).textTheme.headlineSmall,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
