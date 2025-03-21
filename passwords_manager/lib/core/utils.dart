f10321b1736177fa14535bbe5fd5a58b920c3d84
fatmakhadidja <154927228+fatmakhadidja@users.noreply.github.com>
2025-03-21 15:04:20 +0100

    set the dark mode for home page

--- passwords_manager/lib/core/utils.dart
+++ passwords_manager/lib/core/utils.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:passwords_manager/theme/theme_constants.dart';
 
-
 class CountButton extends StatefulWidget {
   final Color clr;
   final Function(int) onNext;
-  final int index ;
-  const CountButton({super.key,required this.onNext, required this.index,required this.clr});
+  final int index;
+  const CountButton({
+    super.key,
+    required this.onNext,
+    required this.index,
+    required this.clr,
+  });
 
   @override
   State<CountButton> createState() => _CountButtonState();
@@ -16,22 +20,21 @@ class _CountButtonState extends State<CountButton> {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-                onPressed: () {
-                  widget.onNext(widget.index-1);
-                },
-                child: Text(
-                  '${widget.index}',
-                  style: TextStyle(
-                    color: widget.clr,
-                    fontFamily: 'BabasNeue',
-                    fontSize: 18,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              );
+      onPressed: () {
+        widget.onNext(widget.index - 1);
+      },
+      child: Text(
+        '${widget.index}',
+        style: TextStyle(
+          color: widget.clr,
+          fontFamily: 'BabasNeue',
+          fontSize: 18,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
   }
 }
-
 
 class PasswordContainer extends StatelessWidget {
   final int number;
@@ -80,6 +83,96 @@ class PasswordContainer extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeSearchBar extends StatefulWidget {
+  final Color borderColor;
+  final FocusNode focusNode;
+  const HomeSearchBar({
+    super.key,
+    required this.focusNode,
+    required this.borderColor,
+  });
+
+  @override
+  State<HomeSearchBar> createState() => _HomeSearchBarState();
+}
+
+class _HomeSearchBarState extends State<HomeSearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return SearchBar(
+      focusNode: widget.focusNode,
+      side: WidgetStateProperty.all(
+        BorderSide(color: widget.borderColor, width: 1),
+      ),
+      backgroundColor: WidgetStateProperty.all(Theme.of(context).primaryColor),
+      leading: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Icon(Icons.search, color: Color(0xffBABABA)),
+      ),
+      hintText: 'Search accounts ..',
+      hintStyle: WidgetStateProperty.all(
+        Theme.of(context).textTheme.headlineSmall,
+      ),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      elevation: WidgetStateProperty.all(0),
+    );
+  }
+}
+
+class AccountContainer extends StatelessWidget {
+  final IconData icon;
+  final String account;
+
+  const AccountContainer({
+    super.key,
+    required this.icon,
+    required this.account,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.101,
+
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xffF1F1F1), width: 2),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.15,
+                  height: MediaQuery.of(context).size.width * 0.15,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).secondaryHeaderColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Icon(icon, color: Theme.of(context).hintColor),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Text('$account', style: Theme.of(context).textTheme.titleLarge),
+              ],
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.copy, color: primaryColor),
+            ),
+          ],
         ),
       ),
     );
--- passwords_manager/lib/screens/homescreen.dart
+++ passwords_manager/lib/screens/homescreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:passwords_manager/theme/theme_constants.dart';
+import '../core/utils.dart';
 
+import 'package:passwords_manager/main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,8 +12,149 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedindex = 0;
+
+  List<Map<String, dynamic>> accounts = [
+    {'icon': Icons.facebook, 'account': 'Facebook'},
+    {'icon': Icons.facebook, 'account': 'Facebook'},
+    {'icon': Icons.facebook, 'account': 'Facebook'},
+    {'icon': Icons.facebook, 'account': 'Facebook'},
+    {'icon': Icons.facebook, 'account': 'Facebook'},
+    {'icon': Icons.facebook, 'account': 'Facebook'},
+  ];
+  FocusNode _focusNode = FocusNode();
+  Color borderColor = Color(0xffF1F1F1);
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen for focus changes
+    _focusNode.addListener(() {
+      setState(() {
+        borderColor = _focusNode.hasFocus ? primaryColor : Color(0xffF1F1F1);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('assets/images/logo.png'),
+                    ),
+                  ),
+
+                  Switch(
+                    value: themeManager.themeMode == ThemeMode.dark,
+                    onChanged: (bool isDark) async {
+                      await themeManager.toggleTheme(isDark);
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  PasswordContainer(number: 5, text: 'Stored \n passwords'),
+                  PasswordContainer(number: 0, text: 'Generated\n passwords'),
+                ],
+              ),
+              SizedBox(height: 20),
+              HomeSearchBar(focusNode: _focusNode, borderColor: borderColor),
+              SizedBox(height: 15),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children:
+                        accounts.map((account) {
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                            child: AccountContainer(
+                              icon: account['icon']!,
+                              account: account['account']!,
+                            ),
+                          );
+                        }).toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        elevation: 5,
+        color: Theme.of(context).primaryColor, // Light grey background
+
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedindex = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.home,
+                  color:
+                      _selectedindex == 0
+                          ? primaryColor
+                          : Theme.of(context).secondaryHeaderColor,
+                  size: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _selectedindex = 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.person,
+                  color:
+                      _selectedindex == 1
+                          ? primaryColor
+                          : Theme.of(context).secondaryHeaderColor,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.redAccent, // Red color
+        child: Icon(Icons.add, size: 30, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
