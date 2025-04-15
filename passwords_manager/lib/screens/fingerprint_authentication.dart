021b8fa1f0a884c68370a59d0a7f4e20bf16c058
fatmakhadidja <154927228+fatmakhadidja@users.noreply.github.com>
2025-04-15 16:50:04 +0100

    fixed overflow

--- passwords_manager/lib/screens/fingerprint_authentication.dart
+++ passwords_manager/lib/screens/fingerprint_authentication.dart
@@ -15,85 +15,87 @@ class _FingerprintAuthPageState extends State<FingerprintAuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: Row(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Text(
+                                'Skip',
+                                style: TextStyle(
+                                  color: primaryColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'BabasNeue',
+                                ), // Customize text color
+                              ),
+                              SizedBox(width: 4),
+                              Icon(Icons.arrow_right_alt, color: primaryColor),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'ENABLE\n',
+                          style: Theme.of(context).textTheme.headlineLarge,
                           children: [
-                            Text(
-                              'Skip',
+                            TextSpan(
+                              text: 'FINGERPRINT\n',
                               style: TextStyle(
                                 color: primaryColor,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
                                 fontFamily: 'BabasNeue',
-                              ), // Customize text color
+                                fontSize: 64,
+                                fontWeight: FontWeight.normal,
+                              ),
                             ),
-                            SizedBox(width: 4),
-                            Icon(Icons.arrow_right_alt, color: primaryColor),
+                            TextSpan(
+                              text: 'LOGIN.',
+                              style: Theme.of(context).textTheme.headlineLarge,
+                            ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
-                   SizedBox(height: MediaQuery.of(context).size.height * 0.1,),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'ENABLE\n',
-                        style: Theme.of(context).textTheme.headlineLarge,
-                        children: [
-                          TextSpan(
-                            text: 'FINGERPRINT\n',
-                            style: TextStyle(
-                              color: primaryColor,
-                              fontFamily: 'BabasNeue',
-                              fontSize: 64,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                          TextSpan(
-                            text: 'LOGIN.',
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
-                        ],
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: Text(
+                        'Skip the typing. Secure your access with just a touch. OneTap Login keeps your data safe and gets you in faster.',
+                        style: Theme.of(context).textTheme.headlineSmall,
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Text(
-                      'Skip the typing. Secure your access with just a touch. OneTap Login keeps your data safe and gets you in faster.',
-                      style: Theme.of(context).textTheme.headlineSmall,
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ColoredButton(
+                      text: 'Login with fingerprint',
+                      whenPressed: () {
+                        authenticate(context, auth);
+                      },
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ColoredButton(
-                    text: 'Login with fingerprint',
-                    whenPressed: () {
-                      authenticate(context, auth);
-                    },
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
